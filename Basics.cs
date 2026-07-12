f036ce135ba6cd8b4dd3add2d6039cf152c84ce9
Selase Apietu <selaseeapietu@gmail.com>
2026-07-12 03:04:58 +0000

    Getting started with Csharp

---
+++ Basics.cs
@@ -0,0 +1,24 @@
+﻿//Hi My name is Selase Apietu. 
+//Welcome to my journey of learning C# to become a software Engineer.
+
+//Printing output on the console
+
+Console.WriteLine("Hello World"); //This command adds a line feed at the end of your output.
+
+Console.Write("My name is ");
+Console.Write("selase."); //This command prints out outputs on the same line.
+
+//Data Types
+
+//A "literal Character" is use to print a single aphanumeric character to the output
+
+Console.WriteLine('B');// writing this "Console.WriteLine('BSE')" will produce an error
+string message = 'C';
+Console.WriteLine(message);
+
+//A "literal int"
+
+
+Random dice = new Random();
+int roll = dice.Next(1, 7);
+Console.WriteLine("You rolled a " + roll); 
--- obj/Debug/net10.0/MicrosoftLearn.AssemblyInfo.cs
+++ obj/Debug/net10.0/MicrosoftLearn.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MicrosoftLearn")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30aa55aae4becc9e41d0c37c20d2be90ba3aa907")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53f25480eb1600c41083eac1d6fab463e03a2c79")]
 [assembly: System.Reflection.AssemblyProductAttribute("MicrosoftLearn")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MicrosoftLearn")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
